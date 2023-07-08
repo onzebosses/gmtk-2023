9e104c67d783e5c5577779f5b3e4e40cd50cd6ad
JulioFonico <jules.berrod@gmail.com>
2023-07-08 12:39:05 +0200

    base sound project commit

---
+++ Tilemap_tuto/Assets/Scripts/Audio/PlayerFootsteps.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class PlayerFootsteps : MonoBehaviour
+{
+    public bool isMoving;
+
+    public bool isGrounded;
+
+    private EventInstance playerFootsteps;
+
+    private void Start()
+    {
+        playerFootsteps = AudioManager.instance.CreateInstance(FMODEvents.instance.playerFootsteps)
+    }
+
+    private void UpdateSound()
+    {
+        if (isMoving && isGrounded)
+        {
+            PLAYBACK_STATE playbackState;
+            playerFootsteps.getPlaybackState(out playbackState);
+            if (playbackState.Equals(PLAYBACK_STATE.STOPPED))
+            {
+                playerFootsteps.start();
+            }
+        }
+
+        else
+        {
+            playerFootsteps.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        }
+    }
+}
